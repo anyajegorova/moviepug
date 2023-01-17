0643e859c03362589f45201b2a557f8b11c6d9ae
anyajegorova <anna.jerm@outlook.com>
2023-01-17 17:06:59 +0200

    added helmet

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.set('view engine', 'pug');
 let movies = [
